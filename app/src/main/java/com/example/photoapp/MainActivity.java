eaf61f5cce6f6660ce3d3daa8d2eed914706526d
Avinash <avinash.paluri@yahoo.com>
2023-04-12 09:04:19 -0400

    Added new folder and new model java files, updated xml home

--- app/src/main/java/com/example/photoapp/MainActivity.java
+++ app/src/main/java/com/example/photoapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.photoapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.home_title);
+
+        Button createAlbumButton = findViewById(R.id.create_album_button);
+        createAlbumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle create album button click
+            }
+        });
+
+        Button deleteAlbumButton = findViewById(R.id.delete_album_button);
+        deleteAlbumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle delete album button click
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/photoapp/model/Tag.java
@@ -0,0 +1,4 @@
+package com.example.photoapp.model;
+
+public class Tag {
+}
